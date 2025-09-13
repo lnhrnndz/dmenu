029f716e41ac268d851f6490a2f070f7f7a0ebe3
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2025-09-13 10:22:26 +0200

    font: change to JetBrains Mono Nerd Font

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono:size=12"
+	"JetBrains Mono Nerd Font:size=12"
 };
 static const char *prompt      = " ";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
